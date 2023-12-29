502bf01796b3c6bbc16a65bd5a7b71bc43d94c66
Oussema Ouakad <81573320+Godyrex@users.noreply.github.com>
2023-12-29 20:46:44 +0100

    Update ManagerXpressBackApplicationTests.java

--- ManagerXpressBack/src/test/java/com/example/managerxpressback/ManagerXpressBackApplicationTests.java
+++ ManagerXpressBack/src/test/java/com/example/managerxpressback/ManagerXpressBackApplicationTests.java
@@ -3,7 +3,6 @@ package com.example.managerxpressback;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ManagerXpressBackApplicationTests {
 
     @Test
