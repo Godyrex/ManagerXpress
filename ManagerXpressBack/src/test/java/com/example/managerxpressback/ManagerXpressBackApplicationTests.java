2e498f27cd0aebad4cfaae7b0048b34d7ada5b12
Oussema Ouakad <81573320+Godyrex@users.noreply.github.com>
2023-12-29 12:44:34 +0100

    testing jacoco

---
+++ ManagerXpressBack/src/test/java/com/example/managerxpressback/ManagerXpressBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.managerxpressback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerXpressBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
